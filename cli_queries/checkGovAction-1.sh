6920ed35fda25182d86b17d69938935adbc3494d
HephyPool <82511049+hephy-io@users.noreply.github.com>
2025-07-21 19:45:31 +0100

    Update checkGovAction-1.sh

--- cli_queries/checkGovAction-1.sh
+++ cli_queries/checkGovAction-1.sh
@@ -1,3 +1,5 @@
+GOV_ACTION_ID="<insert id here>"
+
 cardano-cli conway query gov-state --mainnet | \
-jq -r --arg govActionId "8ad3d454f3496a35cb0d07b0fd32f687f66338b7d60e787fc0a22939e5d8833e" \
+jq -r --arg govActionId ${GOV_ACTION_ID} \
 '.proposals | to_entries[] | select(.value.actionId.txId == $govActionId and .value.actionId.govActionIx == 1) | .value'
